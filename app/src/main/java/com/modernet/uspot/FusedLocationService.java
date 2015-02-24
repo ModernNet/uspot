d6a92e407a29603fd8e735a815c8d2db16bd2fa0
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2015-02-24 16:54:44 +0100

    com.modernet.uspot

---
+++ app/src/main/java/com/modernet/uspot/FusedLocationService.java
@@ -0,0 +1,102 @@
+package com.modernet.uspot;
+
+import android.app.Activity;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class FusedLocationService implements
+        LocationListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+    private static final String TAG = "FusedLocationService";
+    private static final long INTERVAL = 1000 * 30;
+    private static final long FASTEST_INTERVAL = 1000 * 5;
+    private static final long ONE_MIN = 1000 * 60;
+    private static final long REFRESH_TIME = ONE_MIN * 5;
+    private static final float MINIMUM_ACCURACY = 50.0f;
+    Activity activity;
+    private LocationRequest locationRequest;
+    public GoogleApiClient googleApiClient;
+    private Location location;
+    private FusedLocationProviderApi fusedLocationProviderApi = LocationServices.FusedLocationApi;
+
+    public FusedLocationService(Activity activity) {
+        this.activity = activity;
+        buildGoogleApiClient();
+
+        googleApiClient.connect();
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(activity)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        /*Location currentLocation = fusedLocationProviderApi.getLastLocation(googleApiClient);
+        location = currentLocation;
+            fusedLocationProviderApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+            // Schedule a Thread to unregister location listeners
+            Executors.newScheduledThreadPool(1).schedule(new Runnable() {
+                @Override
+                public void run() {
+                    fusedLocationProviderApi.removeLocationUpdates(googleApiClient,
+                            FusedLocationService.this);
+                }
+            }, ONE_MIN, TimeUnit.MILLISECONDS);*/
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        this.location = location;
+        //if the existing location is empty or
+        //the current location accuracy is greater than existing accuracy
+        //then store the current location
+        if (null == this.location || location.getAccuracy() < this.location.getAccuracy()) {
+            this.location = location;
+            //if the accuracy is not better, remove all location updates for this listener
+            if (this.location.getAccuracy() < MINIMUM_ACCURACY) {
+                fusedLocationProviderApi.removeLocationUpdates(googleApiClient, this);
+            }
+        }
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection has been interrupted.
+        // Disable any UI components that depend on Google APIs
+        // until onConnected() is called.
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // This callback is important for handling errors that
+        // may occur while attempting to connect with Google.
+        Toast.makeText(activity, "Connection with Google Services failed.", Toast.LENGTH_SHORT).show();
+        Log.e(TAG,"Connection with Google Services failed.");
+    }
+}
